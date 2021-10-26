c91669fbccdfd36c3efc1606bc66ffa6546cf14a
kaleem <ks@softwarealliance.dk>
2021-10-26 18:50:49 +0500

    First commit

--- app/controllers/subjectController.js
+++ app/controllers/subjectController.js
@@ -1,11 +1,15 @@
 const axios= require('axios')
-
+const apiResponse=require('../helpers/apiResponse')
 
 exports.subjects= async (req, res, next) => {
-    const result= await axios.get('http://ec2-54-67-14-59.us-west-1.compute.amazonaws.com:3000/api/subject/getAllSubjects')
-    console.log(result.data)
+    try{
+        const result= await axios.get('http://ec2-54-67-14-59.us-west-1.compute.amazonaws.com:3000/api/subject/getAllSubjects')
 
-    res.send({
-        result: result.data
-    })
+        res.send({
+            result: result.data
+        })
+    }
+    catch (error){
+        res.send(apiResponse.error(error, null, error.status))
+    }
 }
